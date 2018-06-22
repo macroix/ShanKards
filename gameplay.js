9f7c29efb39b7603ccc7d3f49bca692012da3253
unknown <macroix@uw.edu>
2018-06-22 23:40:42 +0100

    create tone selector tables

--- gameplay.js
+++ gameplay.js
@@ -13,6 +13,7 @@ const dictionary = '{' +
       '"flat_pinyin": "ai",' +
       '"desc": "feeling",' +
       '"tones": [4],' +
+      '"length": [2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -22,6 +23,7 @@ const dictionary = '{' +
       '"flat_pinyin": "ba",' +
       '"pos": "number",' +
       '"tones": [1],' +
+      '"length": [2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -31,6 +33,7 @@ const dictionary = '{' +
       '"flat_pinyin": "baba",' +
       '"pos": "family",' +
       '"tones": [4, 5],' +
+      '"length": [2, 2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -40,6 +43,7 @@ const dictionary = '{' +
       '"flat_pinyin": "beizi",' +
       '"pos": "object",' +
       '"tones": [1, 5],' +
+      '"length": [3, 2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -49,6 +53,7 @@ const dictionary = '{' +
       '"flat_pinyin": "Beijing",' +
       '"pos": "place",' +
       '"tones": [3, 1],' +
+      '"length": [3, 4],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -58,6 +63,7 @@ const dictionary = '{' +
       '"flat_pinyin": "ben",' +
       '"pos": "mw",' +
       '"tones": [3],' +
+      '"length": [3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -67,6 +73,7 @@ const dictionary = '{' +
       '"flat_pinyin": "beizi",' +
       '"pos": "object",' +
       '"tones": [1, 5],' +
+      '"length": [3, 2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -76,6 +83,7 @@ const dictionary = '{' +
       '"flat_pinyin": "by keqi",' +
       '"pos": "phrase",' +
       '"tones": [2, 4, 5],' +
+      '"length": [2, 2, 2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -85,6 +93,7 @@ const dictionary = '{' +
       '"flat_pinyin": "bu",' +
       '"pos": "syntax",' +
       '"tones": [4],' +
+      '"length": [2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -94,6 +103,7 @@ const dictionary = '{' +
       '"flat_pinyin": "cai",' +
       '"pos": "food",' +
       '"tones": [4],' +
+      '"length": [3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -103,6 +113,7 @@ const dictionary = '{' +
       '"flat_pinyin": "cha",' +
       '"pos": "food",' +
       '"tones": [2],' +
+      '"length": [3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -112,6 +123,7 @@ const dictionary = '{' +
       '"flat_pinyin": "chi",' +
       '"pos": "action",' +
       '"tones": [1],' +
+      '"length": [3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -121,6 +133,7 @@ const dictionary = '{' +
       '"flat_pinyin": "chuzuche",' +
       '"pos": "object",' +
       '"tones": [1, 1, 1],' +
+      '"length": [3, 2, 3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -130,6 +143,7 @@ const dictionary = '{' +
       '"flat_pinyin": "da dianhua",' +
       '"pos": "action",' +
       '"tones": [3, 4, 4],' +
+      '"length": [2, 4, 3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -139,6 +153,7 @@ const dictionary = '{' +
       '"flat_pinyin": "da",' +
       '"pos": "description",' +
       '"tones": [4],' +
+      '"length": [2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -148,6 +163,7 @@ const dictionary = '{' +
       '"flat_pinyin": "de",' +
       '"pos": "syntax",' +
       '"tones": [5],' +
+      '"length": [2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -157,6 +173,7 @@ const dictionary = '{' +
       '"flat_pinyin": "dian",' +
       '"pos": "object",' +
       '"tones": [3],' +
+      '"length": [4],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -166,6 +183,7 @@ const dictionary = '{' +
       '"flat_pinyin": "diannao",' +
       '"pos": "object",' +
       '"tones": [4, 3],' +
+      '"length": [4, 3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -175,6 +193,7 @@ const dictionary = '{' +
       '"flat_pinyin": "dianshi",' +
       '"pos": "object",' +
       '"tones": [4, 4],' +
+      '"length": [4, 3],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -184,6 +203,7 @@ const dictionary = '{' +
       '"flat_pinyin": "dianying",' +
       '"pos": "object",' +
       '"tones": [4, 3],' +
+      '"length": [4, 4],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -193,6 +213,7 @@ const dictionary = '{' +
       '"flat_pinyin": "dongxi",' +
       '"pos": "object",' +
       '"tones": [1, 5],' +
+      '"length": [4, 2],' +
       '"hsk": 1' +
     '},' +
     '{' +
@@ -202,6 +223,7 @@ const dictionary = '{' +
       '"flat_pinyin": "dou",' +
       '"pos": "syntax",' +
       '"tones": [1],' +
+      '"length": [3],' +
       '"hsk": 1' +
     '}' +
   ']' +
@@ -330,6 +352,12 @@ const escalate = function() {
 			});
 		} else {
 			this.style.backgroundColor = '#ffd3d5';
+			var domNode = anime({
+  				targets: ".choice",
+  				translateX: 800,
+  				delay: 1000,
+  				easing: 'easeInOutQuart'
+			});
 		}
 		setTimeout(reframeEscalate, 1500);
 	}
@@ -387,15 +415,31 @@ const checkInput = function() {
 	for (let i=0;i<textInputs.length;i++) {
 		userAnswer += textInputs[i].value;
 	}
-	if (userAnswer === dealer.entries[currentWord].flat_pinyin.replace(/\s/g,'')) {
+	if (userAnswer.toLowerCase() === dealer.entries[currentWord].flat_pinyin.replace(/\s/g,'').toLowerCase()) {
 		incrementScore();
+		var cssProperties = anime({
+  			targets: '#score-board',
+  			translateX: '25',
+  			easing: 'easeInOutQuad',
+  			direction: 'alternate',
+  			duration: 1000
+		});
+		//setTimeout(reframe2, 400); /////////////////////////////////////////// Key to Stage 2
+		setTimeout(toneEscalate, 400); ///////////////////////////////////////// Key to Stage 3
+	} else {
+		var cssProperties = anime({
+  			targets: textInputs,
+  			backgroundColor: '#ffd3d5',
+  			translateY: 50,
+  			rotate: 30,
+  			easing: 'easeInOutQuad'
+		});
+		setTimeout(reframe2, 400);
 	}
-	setTimeout(reframe2, 400);
 }
 
 const reframeEscalate = function() {
     enabled = true;
-
 	syllableCount = dealer.entries[currentWord].tones.length
 	while (buttonContainer.firstChild) {
 		buttonContainer.removeChild(buttonContainer.firstChild);
@@ -413,11 +457,112 @@ const reframeEscalate = function() {
 	textInputs[0].focus()
 }
 
+///////////////////// MODE 3 ////////////////////////
+
+const vowelDict = {
+  'a': ['ā', 'á', 'ǎ', 'à', 'a'],
+  'e': ['ē', 'é', 'ě', 'è', 'e'],
+  'i': ['ī', 'í', 'ǐ', 'ì', 'i'],
+  'o': ['ō', 'ó', 'ǒ', 'ò', 'o'],
+  'u': ['ū', 'ú', 'ǔ', 'ù', 'u']
+}
+
+const findVowel = function(syl) {
+  if (syl.includes('iu')) {
+    return('u');
+  } else if (syl.includes('io')) {
+    return('o');
+  } else if (syl.includes('a')) {
+    return('a');
+  } else if (syl.includes('e')) {
+    return('e');
+  } else if (syl.includes('i')) {
+    return('i');
+  } else if (syl.includes('o')) {
+    return('o');
+  } else {
+    return('u');
+  }
+}
+
+const sumToIndex = function(list, i) {
+  sum = 0;
+  for (let j=0;j<i;j++) {
+    sum+=list[j];
+  }
+  return(sum);
+}
+
+const makeTable = function(term, length) {
+  let table = document.getElementsByTagName('table')[0];
+  term = term.replace(/\s/g,'')
+  for (let i=0;i<length.length; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    table.appendChild(row);
+    let syl = document.createElement("div");
+    syl.className = "cell";
+    let pin = term.substring(sumToIndex(length, i), sumToIndex(length, i) + length[i])
+    let vowel = vowelDict[findVowel(pin)];
+    for (let i=0;i<vowel.length;i++) {
+      let vo = document.createElement("div");
+      vo.className = "cell";
+      let vot = document.createTextNode(pin.replace(findVowel(pin),vowel[i]));
+      vo.addEventListener('click', chooseTone, false);
+      vo.addEventListener('click', compareTone, false);
+      vo.appendChild(vot);
+      row.appendChild(vo);
+    }
+  }
+}
+
+const chooseTone = function() {
+  let siblings = this.parentNode.childNodes
+  for (let i=0;i<siblings.length;i++) {
+    siblings[i].classList.remove('marked');
+  }
+  this.classList.toggle('marked');
+}
+
+const compareTone = function() {
+	let selections = document.getElementsByClassName('marked');
+	let tones = dealer.entries[currentWord].tones;
+	let input = [];
+	if (selections.length === tones.length) {
+		for (let i=0; i<selections.length; i++) {
+			let row = selections[i].parentNode;
+			let index = Array.prototype.indexOf.call(row.children, selections[i]);
+			input.push(index + 1);
+		}
+		let flag = true;
+		for (let i=0; i<input.length; i++) {
+			if (input[i] !== tones[i]) {
+				flag = false;
+			}
+		} 
+		if (flag) {
+			incrementScore();
+		}
+	}
+}
+
+const toneEscalate = function() {
+	while (buttonContainer.firstChild) {
+		buttonContainer.removeChild(buttonContainer.firstChild);
+	}
+	let table = document.createElement('table');
+	buttonContainer.appendChild(table);
+	makeTable(dealer.entries[currentWord].flat_pinyin, dealer.entries[currentWord].length);
+}
+
 ///////////////////// MAIN ////////////////////////
 
 let answerButtons = document.getElementsByClassName('choice');
 let enabled = true;
 
+// Changes Mode:
+// for Mode 1, function is answerCheck
+// for Mode 2, function is escalate
 for (let i=0; i<answerButtons.length; i++) {
     answerButtons[i].addEventListener('click', escalate, false);
 }
